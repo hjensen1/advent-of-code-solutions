b26fe9138ad2897cf5d1c40176f7beceb41bb70e
Hayden Jensen <hayden@Haydens-MacBook-Pro.local>
2020-12-01 21:10:05 -0800

    2020 02

--- 2020/02a.rb
+++ 2020/02a.rb
@@ -1,8 +1,18 @@
 load '../util.rb'
 
-File.readlines('./02.txt').each do |line|
-
+def valid?(line)
+  parts = line.split
+  min, max = parts[0].split('-').map(&:to_i)
+  char = parts[1][0]
+  string = parts[2]
+  num = string.split('').count { |x| x == char}
+  num >= min && num <= max
 end
 
-puts
+count = 0
+File.readlines('./02.txt').each do |line|
+  count += 1 if valid?(line)
+end
+
+puts count
 puts "Finished in #{Time.now - @start_time} seconds."
--- 2020/02b.rb
+++ 2020/02b.rb
@@ -1,8 +1,17 @@
 load '../util.rb'
 
-File.readlines('./02.txt').each do |line|
-
+def valid?(line)
+  parts = line.split
+  min, max = parts[0].split('-').map(&:to_i)
+  char = parts[1][0]
+  string = parts[2]
+  (string[min - 1] == char) ^ (string[max - 1] == char)
 end
 
-puts
+count = 0
+File.readlines('./02.txt').each do |line|
+  count += 1 if valid?(line)
+end
+
+puts count
 puts "Finished in #{Time.now - @start_time} seconds."
