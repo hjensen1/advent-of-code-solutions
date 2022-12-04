2177941ae15f3c9750391192490518d740ab467a
hjensen1 <haydenjensen90@gmail.com>
2022-12-03 21:19:03 -0800

    2022 04

---
+++ 2022/04b.rb
@@ -0,0 +1,12 @@
+require '../util.rb'
+
+result = 0
+File.read('./04.txt').split("\n").each do |line|
+  a, b, c, d = line.split(/[-,]/).map(&:to_i)
+  r1 = (a..b).to_a
+  r2 = (c..d).to_a
+  result += 1 unless (r1 & r2).empty?
+end
+
+puts result
+puts "Finished in #{Time.now - @start_time} seconds."
